14fc4811ff6834d48fe6d32f4b897446725e0ecc
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-08 11:12:20 -0300

    lógica do projeto pronta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 function App() {
+  const [input, setInput] = useState("");
+  const [resposta, setResposta] = useState(0);
+
+  function converter() {
+    let valor = 0;
+    let expoenteBase = input.length - 1;
+    if (input.length <= 8) {
+      for (let i = 0; i < input.length; i++) {
+        if (Number(input[i]) === 0 || Number(input[i]) === 1) {
+          if (Number(input[i]) === 1) {
+            valor += Math.pow(2, expoenteBase);
+          }
+          expoenteBase--;
+        } else {
+          alert("O número binário só pode ser composto por 0 e 1.");
+        }
+      }
+    } else {
+      alert("Favor colocar no máximo 8 números!");
+    }
+    setResposta(valor);
+  }
+
   return (
     <div className="App">
-      <h1> Bin 2 Dec</h1>
+      <h1> Binário para Número</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="button" onClick={converter}>
+        Converter
+      </button>
+      <p>Número: {resposta} </p>
     </div>
   );
 }
